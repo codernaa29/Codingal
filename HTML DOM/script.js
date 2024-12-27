ce36b126446b35c3ba73acbc5bc5fedcf5bcce3e
codernaa29 <01717360938hawa@gmail.com>
2024-12-26 21:18:39 -0800

    2nd commit

---
+++ HTML DOM/script.js
@@ -0,0 +1,11 @@
+var heading_text = document.getElementById("myheading").innerHTML;
+alert(heading_text);
+
+function showImage() {
+    document.getElementById("image").src = "https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg";
+
+    document.getElementById("image").style.width = "700px";
+
+}
+
+document.body.style.backgroundColor = "skyblue";
